2862ecd0008884265416603cde882f898b877980
admin <rohitvarshney87@gmail.com>
2024-02-13 15:17:29 +0530

    bubble sort

---
+++ Sorting/bubble_sort.java
@@ -0,0 +1,32 @@
+package Sorting;
+
+import java.util.Arrays;
+
+/**
+ * bubble_sort
+ */
+public class bubble_sort {
+
+    public void bubble_sort(int [] arr){
+        for(int i = 0; i<arr.length-1; i++){
+            Boolean swapped = false;    // Optimization -> in case if the array is already sorted then we don't have to check for whole array
+            for(int j = 0; j<arr.length-i-1; j++){
+                if (arr[j] > arr[j+1]) {
+                    swap(arr, j, j + 1);
+                    swapped = true;
+                }
+            }
+            if(swapped == false){
+                break;
+            }
+        }
+        System.out.println("Via. Bubble Sort ::======>>  "+Arrays.toString(arr));
+    }
+
+    private void swap(int[] arr, int x, int y) {
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+
+}
---
+++ main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+import Sorting.bubble_sort;
+
+public class main {
+    public static void main(String[] args) {
+        int arr[] = {1,3,2,34,234,4,32,15};
+        new bubble_sort().bubble_sort(arr);
+
+    }
+}
