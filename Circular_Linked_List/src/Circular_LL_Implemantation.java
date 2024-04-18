6564f8816be14e95c53a4b1489681ca8cb22c067
admin <rohitvarshney87@gmail.com>
2024-04-18 18:32:55 +0530

    Circular linked list

--- Circular_Linked_List/src/Circular_LL_Implemantation.java
+++ Circular_Linked_List/src/Circular_LL_Implemantation.java
@@ -1,2 +1,31 @@
-public class Circular_LL_Implemantation {
+public  class Circular_LL_Implemantation {
+    public static class Node{
+        int val;
+        Node next;
+        Node(int x){
+            this.val = x;
+            next = null;
+        }
+    }
+
+    public static void printLinkedListData(Node head){
+//        Node curr = head.next;
+//        System.out.print(head.val);
+//        while(curr != head){
+//            System.out.print("->"+curr.val );
+//            curr = curr.next;
+//        }
+
+        System.out.print(head.val);
+        for(Node curr = head.next; curr != head; curr = curr.next){
+            System.out.print("->"+curr.val );
+        }
+
+
+
+    }
+
+
 }
+
+
--- Circular_Linked_List/src/Main.java
+++ Circular_Linked_List/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Circular_LL_Implemantation.Node head = new Circular_LL_Implemantation.Node(10);
+        Circular_LL_Implemantation.Node temp1 = new Circular_LL_Implemantation.Node(11);
+        Circular_LL_Implemantation.Node temp2 = new Circular_LL_Implemantation.Node(12);
+
+        head.next = temp1;
+        temp1.next = temp2;
+        temp2.next = head;
+
+        Circular_LL_Implemantation.printLinkedListData(head);
+
+
     }
 }
