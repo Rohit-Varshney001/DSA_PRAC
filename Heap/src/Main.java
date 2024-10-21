25e9ceb7e95809e54ff296430da7aa9f5b2b7f63
admin <rohitvarshney87@gmail.com>
2024-10-21 23:44:19 +0530

    Heap Pairs with class

--- Heap/src/Main.java
+++ Heap/src/Main.java
@@ -11,6 +11,9 @@ public class Main {
         System.out.println("kth Smallest---> "+kSmallest(arr,3));
         System.out.println("k closest---> "+ Arrays.toString( kClosestNumber(arr2,3,4)));
         System.out.println("k Frequent Numbers---> "+ Arrays.toString( kFrequentNumbers(new int[]{1,2,3,1,2,2,1}, 2)));
+        System.out.println("k Closest Points To Origin ---> "+ Arrays.deepToString( kClosestPointsToOrigin(new int[][]{{1, 2}, {3, 1}, {2, 2}}, 2)));
+        System.out.println("connect Ropes To Min Length---> "+ connectRopesToMinLength(new int[]{1,2,3,4,5}));
+        System.out.println("Sum Between K1 and K2 Smallest---> "+ sumBetweenK1andK2Smallest(new int[]{1,2,3,4,5},2,4));
     }
 
     public static int kLargest(int[] arr, int k) {
@@ -106,4 +109,72 @@ public class Main {
 
         return res;
     }
+    static class Pair {
+        int x;
+        int y;
+        long sqDist;
+
+        Pair(int x, int y, long sqDist) {
+            this.x = x;
+            this.y = y;
+            this.sqDist = sqDist;
+        }
+    }
+
+    public static int[][] kClosestPointsToOrigin(int [][] arr, int k){
+        PriorityQueue<Pair> maxHeap = new PriorityQueue<>((a, b) -> Long.compare(b.sqDist, a.sqDist));
+        int [][] res = new int[k][2];
+
+        for (int[] point : arr) {
+            int x = point[0];
+            int y = point[1];
+            long sqDist = (long) x * x + (long) y * y;
+            maxHeap.add(new Pair(x, y, sqDist));
+
+            // If heap size exceeds k, remove the farthest point
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
+            }
+        }
+        int i = 0;
+        while(!maxHeap.isEmpty()){
+            Pair pair = maxHeap.poll();
+            res[i][0] = pair.x;
+            res[i][1] = pair.y;
+            i++;
+        }
+        return res;
+
+    }
+
+    public static int connectRopesToMinLength(int [] arr){
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(int i : arr){
+            minHeap.add(i);
+        }
+        int totalCost = 0;
+        while (minHeap.size()>1){
+            int rp1 = minHeap.poll();
+            int rp2 = minHeap.poll();
+            int sum = rp1+rp2;
+            totalCost += sum;
+            minHeap.add(sum);
+        }
+        return totalCost;
+    }
+    public static int sumBetweenK1andK2Smallest(int[] arr, int k1, int k2) {
+        // Finding k1-th and k2-th smallest elements
+        int k1Smallest = kSmallest(arr, k1);
+        int k2Smallest = kSmallest(arr, k2);
+
+        // Calculate the sum of elements between k1-th and k2-th smallest
+        int sum = 0;
+        for (int num : arr) {
+            if (num > k1Smallest && num < k2Smallest) {
+                sum += num;
+            }
+        }
+        return sum;
+    }
+
 }
